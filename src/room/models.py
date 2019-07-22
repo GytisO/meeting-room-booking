359678cc7bcfebac9d5dbee4c1b439164380281e
Gytis Ožiūnas <gytis@oziunas.com>
2019-07-22 10:01:31 +0300

    comments deleted

--- src/room/models.py
+++ src/room/models.py
@@ -4,16 +4,3 @@
 
 
 # # Create your models here.
-
-# User = settings.AUTH_USER_MODEL
-
-
-# class MeetingRoom(models.Model):
-#     user = models.ForeignKey(User, default=1, null=True,
-#                              on_delete=models.SET_NULL)
-#     room_number = models.IntegerField(unique=True, blank=False)
-#     capacity = models.IntegerField(blank=False)
-#     # models.One
-
-#     def get_absolute_url(self):
-#         return reverse('room-details', kwargs={'room_number': self.room_number})
