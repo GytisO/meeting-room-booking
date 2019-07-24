f7821f89f42090bd0811fa1534f66823017dd709
GytisO <gytis@oziunas.com>
2019-07-24 08:10:44 +0300

    user save undo

--- src/API/serializers.py
+++ src/API/serializers.py
@@ -24,6 +24,18 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
                         {"write_only": True}
                         }
 
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+            username=username,
+            email=email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
+
 
 class RoomSerializer(serializers.ModelSerializer):
     room_number = serializers.IntegerField()
